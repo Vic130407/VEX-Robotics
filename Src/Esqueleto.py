875aa66ebd275e82ea99941aa62125c01357f328
Dennnis Soto <blancarubio9430@gmail.com>
2025-08-29 14:12:56 -0600

    condicional menu

--- Src/Esqueleto.py
+++ Src/Esqueleto.py
@@ -8,6 +8,8 @@ print("4. Barra de progreso")
 print("5. Salir")
 opcion = input("Seleccione una opción: ")
 
+#condicional
+if opcion == 1:
 # Integrantes por sector
 # Preguntar nombres de integrantes de Administración
 administracion = input("Escribe los integrantes del sector Administración (separados por coma): ")
@@ -15,14 +17,51 @@ administracion = input("Escribe los integrantes del sector Administración (sepa
 mecanica = input("Escribe los integrantes del sector Mecánica (separados por coma): ")
 # Preguntar nombres de integrantes de Programación
 programacion = input("Escribe los integrantes del sector Programación (separados por coma): ")
-
+#agg integrante a cualquier seccion
+nuevo_integrante= input("Deseas agregar un integrante nuevo (s/n)")
+if nuevo_integrante == "s":
+     input("Escribe aqui el nombre del nuevo integrante->")
+    
 # -------------------------------
 #variables de materiales 
+Motores=0
+Cerebros=0
+Cable=0
+Tornillos=0
+Tuercas=0
+Arandelas=0
+Baterias=0
+Canales=0
+Placas_Acero=0
+Chasis_Acero=0
+Barras_Acero=0
+Steel_Angles=0
+CCanales_Acero=0
+Refuerzos_Acero=0
+Engranajes=0
+Rodillos=0
+Embragues=0
+Collares=0
+Ejes=0
+Separadores=0
+Espaciadores=0
+
+
+
 
 # Inventario de materiales
 # Preguntar materiales disponibles
 materiales = input("Escribe los materiales con sus cantidades (ejemplo: Tornillos=50, Motores=2): ")
 
+#opcion de agregar o eliminar materiales
+agregarMaterial= input ("Desea agregar material al invenario?(s/n)")
+if agregarMaterial =="s":
+    input("Escribe el nombre del material que desea agregar")
+
+sacarMaterial= input("Desea sacar material del inventario?(s/n)")
+if sacarMaterial=="s":
+    input("Escribe el nombre del material que se desea sacar")
+ 
 # Lista de pendientes: lista de tareas con su estado ()
 pendientes = (
     ("Diseñar prototipo", False),
