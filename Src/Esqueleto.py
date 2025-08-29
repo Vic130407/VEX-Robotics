b70fc55a6586fa98e5a30cc9976d879b1560f851
Nicolas <nickfloresverdad@gmail.com>
2025-08-28 20:25:23 -0600

    Le avance algo al esqueleto porfa checar :)

--- Src/Esqueleto.py
+++ Src/Esqueleto.py
@@ -1,2 +1,22 @@
+# VEX ROBOTICS - GESTOR DE PROYECTOS
+
+#-------------------------------
 #inicia el programa
-# variables
+
+#-------------------------------
+# variables de integrantes
+
+# Integrantes por sector
+# Preguntar nombres de integrantes de Administración
+administracion = input("Escribe los integrantes del sector Administración (separados por coma): ")
+# Preguntar nombres de integrantes de Mecánica
+mecanica = input("Escribe los integrantes del sector Mecánica (separados por coma): ")
+# Preguntar nombres de integrantes de Programación
+programacion = input("Escribe los integrantes del sector Programación (separados por coma): ")
+
+# -------------------------------
+#variables de materiales 
+
+# Inventario de materiales
+# Preguntar materiales disponibles
+materiales = input("Escribe los materiales con sus cantidades (ejemplo: Tornillos=50, Motores=2): ")
