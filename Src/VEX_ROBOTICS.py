d91a393b3abea9b023fa5ebebc7733ef5b3dace2
Dennnis Soto <blancarubio9430@gmail.com>
2025-09-24 22:17:27 -0600

    hice el menu principal

---
+++ Src/VEX_ROBOTICS.py
@@ -0,0 +1,10 @@
+def menu_tareas():
+    while True:
+        print("\n--- Menu de pendientes ---")
+        print("1. Mostrar tareas")
+        print("2. Agregar tarea")
+        print("3. Marcar tarea como completada")
+        print("4. Ver resumen")
+        print("5. Ver barra de progreso")
+        print("6. Salir")
+        opcion = input("Elige una opcion: ")
